0faea6b0fac28e55f39708850a313ce5df0111fb
Gopichandar <v-gopichandar.s@rksv.in>
2024-06-30 23:35:02 +0530

    instruction updated

--- main.py
+++ main.py
@@ -64,8 +64,7 @@ while True:
 
     # Prompt
     template = """Use the following pieces of context to answer the question at the end.
-    If you don't know the answer, just say that you don't know, don't try to make up an answer.
-    Use three sentences maximum and keep the answer as concise as possible.
+    If you don't know the answer, just say that you don't know, don't try to make up an answer.    
     {context}
     Question: {question}
     Helpful Answer:"""
