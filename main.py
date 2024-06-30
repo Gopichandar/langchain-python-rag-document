d48d271712637f467a2e62bf3b14e4b5cc174eca
Gopichandar <v-gopichandar.s@rksv.in>
2024-06-30 22:08:25 +0530

    model updated

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ while True:
         template=template,
     )
 
-    llm = Ollama(model="llama3:8b", callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]))
+    llm = Ollama(model="llama3", callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]))
     qa_chain = RetrievalQA.from_chain_type(
         llm,
         retriever=vectorstore.as_retriever(),
